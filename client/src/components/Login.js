121a6fbe08de462c96e903258b0c367c79bc89c2
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-05-22 11:39:25 -0400

    created login page still need axiosWithAuth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,67 @@
 import React from "react";
+import { useHistory } from 'react-router-dom'
 
-const Login = () => {
+const push = useHistory()
+class Login extends React.Component{
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+  constructor(){
+    super();
+    this.state = {
+        credentials: {
+            username: '',
+            password: '',
+        }
+    }
+  }
+
+
+  handleChange = e => {
+      this.setState({
+        credentials: {
+          ...this.state.credentials,
+          [e.target.name]: e.target.value
+        }
+      });
+    };
+
+  login = e => {
+      e.preventDefault();
+      axiosWithAuth()
+        .post('/api/login', this.state.credentials)
+        .then(res => {
+            localStorage.setItem('token', res.data.payload);
+            push('/protected');
+        })
+  }
+
+  render(){
+    return (
+      <>
+        <h1>Welcome to the Bubble App!</h1>
+          <form onSubmit={this.login}>
+            {/* username */}
+            <input
+              placeholder='username'
+              type='text'
+              name='username'
+              value={this.state.credentials.username}
+              onChange={this.handleChange}
+            />
+            {/* password */}
+              <input
+                placeholder='password'
+                type='password'
+                name='password'
+                value={this.state.credentials.password}
+                onChange={this.handleChange}
+              />
+              <button>Log in</button>
+            </form>
+      </>
+    );
+  }
+  
 };
 
 export default Login;
