b8e2dfa614b5ca6039c679141176fb2bef9e3733
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-05-22 12:31:30 -0400

    Made BubblePage render on private route, works perfect AND fetched the colors from the API :))))))

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -9,7 +9,13 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
- 
+  useEffect(() => {
+    axiosWithAuth()
+      .get('/api/colors')
+      .then(res => {
+        setColorList(res.data)
+      })
+  }, [])
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
